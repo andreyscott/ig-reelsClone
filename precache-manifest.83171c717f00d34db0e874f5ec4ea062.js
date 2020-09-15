1a1eb359a094246429f4d98cdd78baf0e2ec24bd
andreyscott <62455784+andreyscott@users.noreply.github.com>
2020-09-15 19:45:57 +0100

    Updates

---
+++ precache-manifest.83171c717f00d34db0e874f5ec4ea062.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "97e1872e81fa023d85c00eda19c27483",
+    "url": "/ig-reels/index.html"
+  },
+  {
+    "revision": "253619127b43efde431a",
+    "url": "/ig-reels/static/css/main.6030e31b.chunk.css"
+  },
+  {
+    "revision": "896ae20fc986d9a91130",
+    "url": "/ig-reels/static/js/2.f9dd8aa6.chunk.js"
+  },
+  {
+    "revision": "5f8be6edcb631ea3c834b8459dc2c264",
+    "url": "/ig-reels/static/js/2.f9dd8aa6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "253619127b43efde431a",
+    "url": "/ig-reels/static/js/main.dcfc5f01.chunk.js"
+  },
+  {
+    "revision": "9dd5386bdd8c497aa064",
+    "url": "/ig-reels/static/js/runtime-main.703e94b5.js"
+  }
+]);
